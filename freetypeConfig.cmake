49e435c98f29c426e24e8a9197ff273386482895
hammy <mileslacey@gmail.com>
2020-11-30 19:19:43 -0800

    Fixed build issues with FreeType and Zlib.

--- freetypeConfig.cmake
+++ freetypeConfig.cmake
@@ -4,8 +4,9 @@
 # #####################################
 find_package(Freetype)
 
-    #include_directories(${FREETYPE_INCLUDE_DIRS})
-if (NOT FREETYPE_INCLUDE_DIRS OR NOT FREETYPE_LIBRARIES)
+option(BUILD_FREETYPE "Force FreeType to build from source." OFF)
+
+if (BUILD_FREETYPE OR NOT FREETYPE_INCLUDE_DIRS OR NOT FREETYPE_LIBRARIES)
     message("-- Building Freetype from source")
 
     set(FREETYPE_BRANCH "master" CACHE STRING "Git branch or tag for checking out FreeType.")
@@ -13,40 +14,44 @@ if (NOT FREETYPE_INCLUDE_DIRS OR NOT FREETYPE_LIBRARIES)
 
     # Configure build options
     set(FREETYPE_BUILD_FLAGS
+        -DCMAKE_BUILD_TYPE:STRING=RELWITHDEBINFO
         -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
         -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
         -DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}
         -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
-        -DCMAKE_INSTALL_PREFIX:STRING=${EXTERNAL_PROJECT_PREFIX}
+        -DCMAKE_RC_COMPILER:FILEPATH=${CMAKE_RC_COMPILER}
+        -DCMAKE_INSTALL_PREFIX:FILEPATH=${EXTERNAL_PROJECT_PREFIX}
+        -DCMAKE_SYSTEM_NAME:STRING=${CMAKE_SYSTEM_NAME}
+        -DCMAKE_MAKE_PROGRAM:PATH=${CMAKE_MAKE_PROGRAM}
         -DBUILD_SHARED_LIBS:BOOL=FALSE
-        -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz:BOOL=TRUE)
+        -DCMAKE_DISABLE_FIND_PACKAGE_ZLIB:BOOL=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_BZip2:BOOL=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_PNG:BOOL=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz:BOOL=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_BrotliDec:BOOL=TRUE)
     mark_as_advanced(FREETYPE_BUILD_FLAGS)
 
     # Build FreeType
     ExternalProject_Add(
         FreeType
         PREFIX
-            ${EXTERNAL_PROJECT_PREFIX}
+        ${EXTERNAL_PROJECT_PREFIX}
         GIT_REPOSITORY
-            "https://git.savannah.nongnu.org/r/freetype/freetype2.git"
+        "https://github.com/freetype/freetype2.git"
         GIT_SHALLOW
-            TRUE
+        TRUE
         GIT_PROGRESS
-            TRUE
+        TRUE
         GIT_TAG
-            "${FREETYPE_BRANCH}"
+        "${FREETYPE_BRANCH}"
         UPDATE_COMMAND
-            ${GIT_EXECUTABLE} fetch
+        ${GIT_EXECUTABLE} fetch
         CMAKE_COMMAND
-            ${CMAKE_COMMAND}
+        ${CMAKE_COMMAND}
         CMAKE_CACHE_ARGS
-            ${FREETYPE_BUILD_FLAGS}
-        BUILD_COMMAND
-            ${CMAKE_COMMAND} -E chdir ${EXTERNAL_PROJECT_PREFIX}/src/FreeType-build ${CMAKE_COMMAND} --build . --config ${CMAKE_CFG_INTDIR}
+        ${FREETYPE_BUILD_FLAGS}
         INSTALL_DIR
-            ${EXTERNAL_PROJECT_PREFIX}
-        INSTALL_COMMAND
-        ${CMAKE_COMMAND} -E chdir ${EXTERNAL_PROJECT_PREFIX}/src/FreeType-build ${CMAKE_COMMAND} --build . --config ${CMAKE_CFG_INTDIR} --target install
+        ${EXTERNAL_PROJECT_PREFIX}
     )
 
     # Add the imported library target
@@ -54,7 +59,7 @@ if (NOT FREETYPE_INCLUDE_DIRS OR NOT FREETYPE_LIBRARIES)
     set_target_properties(freetype PROPERTIES IMPORTED_LOCATION ${EXTERNAL_PROJECT_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}freetype${CMAKE_STATIC_LIBRARY_SUFFIX})
     add_dependencies(freetype FreeType)
 
-    set(FREETYPE_INCLUDE_DIRS ${EXTERNAL_PROJECT_PREFIX}/include  ${EXTERNAL_PROJECT_PREFIX}/include/freetype2)
+    set(FREETYPE_INCLUDE_DIRS ${EXTERNAL_PROJECT_PREFIX}/include ${EXTERNAL_PROJECT_PREFIX}/include/freetype2)
     set(FREETYPE_LIBRARIES freetype)
 
 endif()
